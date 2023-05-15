19a2731b06b6b54ebb4b5522e054a093ebe72240
fernandaagustoni <fernanda_agustoni@hotmail.com>
2023-05-15 15:52:06 -0300

    tela inicial

--- app/src/main/java/br/edu/ifsp/app13_mytasks_dmo/view/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/app13_mytasks_dmo/view/MainActivity.java
@@ -1,8 +1,10 @@
-package br.edu.ifsp.app13_mytasks_dmo;
+package br.edu.ifsp.app13_mytasks_dmo.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import br.edu.ifsp.app13_mytasks_dmo.R;
 
 public class MainActivity extends AppCompatActivity {
 
